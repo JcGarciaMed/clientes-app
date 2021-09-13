a74c9b689725eafe2a9c08da0d1f16a946966f58
JORGE <jcgarciamed@gmail.com>
2021-09-13 15:49:03 -0500

    LOADING BOOTSTRAP

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'jorge', apellido:'garcia', email:'jcgarcia@gmail.com', creatAt:'20211211'},
+  {id: 2, nombre: 'carlos', apellido:'zambrano', email:'czam@gmail.com', creatAt:'20211212'},
+  {id: 3, nombre: 'ricardo', apellido:'kaka', email:'rikaka@gmail.com', creatAt:'20211213'},
+  {id: 4, nombre: 'andre', apellido:'carrillo', email:'ancar@gmail.com', creatAt:'20211214'},
+  {id: 5, nombre: 'gianluca', apellido:'lapadula', email:'glapa@gmail.com', creatAt:'20211215'},
+  {id: 6, nombre: 'paolo', apellido:'guerrero', email:'parrero@gmail.com', creatAt:'20211216'}
+];
